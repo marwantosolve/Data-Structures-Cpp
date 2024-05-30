f04c3f5636e15c3e6ffa78e407978a3f42952347
Marwan Osama <me.hack1101@gmail.com>
2024-05-30 20:43:41 +0300

    bubble sort

---
+++ Bubble-Sort.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+void bubbleSort(T arr[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] < arr[j - 1]) {
+        swap(arr[j], arr[j - 1]);
+      }
+    }
+  }
+  return;
+}
